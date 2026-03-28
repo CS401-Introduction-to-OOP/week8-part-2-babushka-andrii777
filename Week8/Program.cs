49fe8d6555a9cf11e3f5d80a5df0ebe034f33979
ANDGRUSHA <friendgrusha@gmail.com>
2026-03-28 15:58:04 +0200

    Colpeted stage 3 and the task 1 overall

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,25 @@
-﻿
+﻿using Ass82;
+
 class Program
 {
     public static void Main()
     {
+        var file = new FileResource("report.txt");
+        var network = new NetworkResource("api.company.local");
+        using(file)
+        {
+            file.Dispose();
+        }
+        using(network)
+        {
+            network.Dispose();
+        }
+        var manager = new ResourceManager<Resource>();
         
+        manager.Add(file);
+        manager.Add(network);
+        manager.OpenAll();
+        manager.CloseAll();
     }
     
 }
